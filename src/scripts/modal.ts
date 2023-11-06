1cdc5e08b88d75da7e12243e5c6cf3cdae374bb0
DeepAung <i.deepaung@gmail.com>
2023-11-06 20:53:53 +0700

    change projects modals + refactor code + styling

--- src/scripts/modal.ts
+++ src/scripts/modal.ts
@@ -10,8 +10,32 @@ modalWrapper.onclick = closeModal;
 export function openModal(element: HTMLElement) {
   if (!modalWrapper) return;
 
-  modalWrapper.append(element.cloneNode(true));
+  modalWrapper.style.overflowY = "scroll";
+  let clonedElem = element.cloneNode(true) as HTMLElement;
+  clonedElem.style.display = "block";
 
+  modalWrapper.append(clonedElem);
+
+  document.body.classList.add("lock-scroll");
+  gsap.to(modalWrapper, {
+    opacity: 1,
+    display: "block",
+    duration: 0.25,
+  });
+}
+
+export function openImageModal(element: HTMLElement) {
+  if (!modalWrapper) return;
+
+  const clonedElem = element.cloneNode(true) as HTMLElement;
+  clonedElem.style.display = "block";
+
+  const img = clonedElem.querySelector("img");
+  img?.classList.add("full-image");
+
+  modalWrapper.append(clonedElem);
+
+  document.body.classList.add("lock-scroll");
   gsap.to(modalWrapper, {
     opacity: 1,
     display: "block",
@@ -22,12 +46,14 @@ export function openModal(element: HTMLElement) {
 export function closeModal() {
   if (!modalWrapper) return;
 
+  document.body.classList.remove("lock-scroll");
   gsap.to(modalWrapper, {
     opacity: 0,
     display: "none",
     duration: 0.25,
     onComplete: () => {
       modalWrapper.innerHTML = "";
+      modalWrapper.style.overflowY = "hidden";
     },
   });
 }
