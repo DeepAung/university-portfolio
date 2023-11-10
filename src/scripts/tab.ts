6ba8d42579a10a01c11c9e8b781e2ef245377b02
DeepAung <i.deepaung@gmail.com>
2023-11-10 18:53:24 +0700

    update Modal and Tab + styling
    - remove close button in Modal
    - move Tab's logic to new file (tab.ts)
    - use Tab instead of Modal in Education section
    - remove "Try Hover/Click Image" text in Intro section

---
+++ src/scripts/tab.ts
@@ -0,0 +1,47 @@
+import gsap from "gsap";
+
+export function initTab(tab: Element, startIndex: number) {
+  console.log(tab);
+
+  const selects = tab?.children[0] as HTMLElement;
+  const contents = tab?.children[1] as HTMLElement;
+
+  let curIndex = startIndex;
+  openAnim(curIndex);
+
+  for (let i = 0; i < selects.children.length; i++) {
+    const child = selects.children[i] as HTMLElement;
+    child.onclick = () => {
+      openTab(i);
+    };
+  }
+
+  function openTab(i: number) {
+    closeAnim(curIndex);
+    curIndex = i;
+    openAnim(curIndex);
+  }
+
+  function openAnim(i: number) {
+    const select = selects.children[i] as HTMLElement;
+    const content = contents.children[i] as HTMLElement;
+
+    select.classList.add("active");
+    gsap.to(content, {
+      opacity: 1,
+      display: "block",
+      ease: "power1.in",
+      duration: 0.25,
+    });
+  }
+
+  function closeAnim(i: number) {
+    const select = selects.children[i] as HTMLElement;
+    const content = contents.children[i] as HTMLElement;
+
+    select.classList.remove("active");
+    gsap.killTweensOf(content);
+    content.style.display = "none";
+    content.style.opacity = "0";
+  }
+}
